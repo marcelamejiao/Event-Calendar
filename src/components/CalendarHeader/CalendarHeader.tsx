ca590055a9c4fce2ceda466ae0bf0c7fd1304594
marcelamejiao <mejiao24@hotmail.com>
2023-12-12 15:58:46 +1100

    implement logic to display a calendar header

--- src/components/CalendarHeader/CalendarHeader.tsx
+++ src/components/CalendarHeader/CalendarHeader.tsx
@@ -1,8 +1,14 @@
+import CalendarCell from "../CalendarCell/CalendarCell";
 
 const CalendarHeader = () => {
 
 	return (
-		<div className="w-[700px] border">
+		<div className="w-[700px] border flex justify-center text-center">
+			<CalendarCell>{"<<"}</CalendarCell>
+			<CalendarCell>{"<"}</CalendarCell>
+			<CalendarCell className="w-[301px]">Month Year</CalendarCell>
+			<CalendarCell>{">"}</CalendarCell>
+			<CalendarCell>{">>"}</CalendarCell>
 		</div>
 	)
 }
