a037e2f11f147a1735a55396decceb0390ef9c91
marcelamejiao <mejiao24@hotmail.com>
2023-12-18 16:07:33 +1100

    add basic modal styling

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,9 +5,10 @@ type modalProps = {
 export const Modal = ({ setShowModal }: modalProps)  => {
 
 	return (
-		<div>
-			<div>
+		<div className="inset-0 fixed bg-gray-100 opacity-80 z-10 flex justify-center items-center overflow-y-hidden">
+			<div className="w-50 flex flex-col bg-neutral-50 items-center justify-center p-5 ">
 				<div>
+					<h1>My calendar modal</h1>
 					<button onClick={setShowModal}>Close</button>
 				</div>
 			</div>
